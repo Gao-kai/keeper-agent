022d69ff9972cf206b6544b02a78592959d129b3
Artest_gaokai <981989629@qq.com>
2026-08-21 23:45:26 +0800

    docs(knowledge): document confirm item name node plan

--- knowledge/processor/query_process/nodes/confirm_item_name_node.py
+++ knowledge/processor/query_process/nodes/confirm_item_name_node.py
@@ -10,12 +10,57 @@ class ConfirmItemNameNode(BaseNode):
 	name = "confirm_item_name_node"
 	
 	def process(self, state: QueryGraphState) -> QueryGraphState:
-		# 获取原始问题从state中
-		# LLM提取 对齐下游基于商品名各路检索 原始问题质量差 口语化 代词 错别字 表达不清楚
+		"""
+		# 1. 获取用户原始输入
+		# 2. 调用LLM提取商品名称
+		# 3. 构建confirmed\options\No的不同分支
+		# 4. 向量检索item_names
+		Args:
+			state:
+
+		Returns:
+
+		"""
+		original_query = state.get("original_query","")
+		
+		
+		
 	
 	
 
 class ItemNameExtractor:
-	pass
+	"""
+	Why?
+	1. 用户模糊、口语化输入统一处理
+	2. 提示词组装后call LLM
+	2. 商品名称提取 单个、多个
+	3. 返回格式JSON校验
+	4. 返回数据清洗 围栏
+	"""
+	def __init__():
+		pass
 	
-	# 1. 获取LLM客户端 构建提示词模版LangChain？
+	def extract_by_llm(self):
+		pass
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
